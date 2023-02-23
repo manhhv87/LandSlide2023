a284e459c678fca107441c734052323e0d98707e
manhhv87 <manhhv87@gmail.com>
2023-02-23 21:36:43 +0700

    modify code

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 
 
@@ -45,3 +46,52 @@ class Evaluator(object):
 
     def reset(self):
         self.confusion_matrix = np.zeros((self.num_class,) * 2)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value.
+
+    Examples::
+        # >>> Initialize a meter to record loss
+        # >>>     losses = AverageMeter()
+        # >>> Update meter after every minibatch update
+        # >>>     losses.update(loss_value, batch_size)
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(logit, target, top_k=(1,), ignore_idx=255):
+    """ Suppose you have the ground truth prediction tensor y of shape b-h-w (dtype=torch.int64).
+        Your modules predict per-pixel class logit of shape b-c-h-w, with c is the number of
+        classes (including "background"). These logit are the "raw" predictions before softmax
+        function transforms them into class probabilities. Since we are only looking at the top k,
+        it does not matter if the predictions are "raw" or "probabilities".
+    """
+    max_k = max(top_k)
+
+    # compute the top k predicted classes, per pixel
+    _, tk = torch.topk(logit, max_k, dim=1)
+
+    # you now have k predictions per pixel, and you want that one of them
+    # will match the true labels target
+    correct_pixels = torch.eq(target[:, None, ...], tk).any(dim=1).float()
+
+    # take the mean of correct_pixels to get the overall average top-k accuracy
+    valid = target != ignore_idx
+    top_k_acc = correct_pixels[valid].mean()
+
+    return top_k_acc
